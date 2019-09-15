ac59d750f1a686acb6a2ea4e273e03bdbc88b10f
Pavel Petrovich <gaara33377@gmail.com>
2019-09-15 23:07:02 +0300

    task 2 update

--- LAB1/TASK2/index.php
+++ LAB1/TASK2/index.php
@@ -1,7 +1,22 @@
 <?php
-
-$today = getdate();
-echo  "{$today['seconds']}";
-
-//$today['hours']}:{$today['minutes']}:
+    $totalSeconds = (int)htmlspecialchars($_POST['seconds']) % 60;
 ?>
+
+<!DOCTYPE html>
+<html lang = 'ru'>
+    <head>
+        <title>TASK 2</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <p>TASK 2</p>
+        <form action = 'index.php' method = 'POST'>
+            <input name = 'seconds' value = '0' type = 'number' />
+            <input type = 'submit' value = 'Получить секунды' />
+        </form>
+        <?php 
+            if (($totalSeconds || $totalSeconds === 0) && $totalSeconds > 0) 
+            echo "<p>Result: {$totalSeconds}</p>"; 
+        ?>
+    </body>
+</html>

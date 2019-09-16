23ef869578646df5b81b477757bd34e413a69338
Pavel Petrovich <gaara33377@gmail.com>
2019-09-16 21:22:55 +0300

    refac task1 lab2

--- LAB2/TASK1_2/index.php
+++ LAB2/TASK1_2/index.php
@@ -1,52 +1,48 @@
 <?php
 
-	function getArea($kaktetA, $kaktetB){
-		return 0.5 * ($kaktetA * $kaktetB);
-	}
-
-	function getResult($areaA = 0, $areaB = 0, $areaC = 0){
+	function getResult($areaA, $areaB, $areaC){
 		if ($areaA > $areaB && $areaA > $areaC) return "Треугольник A, S = {$areaA}";
 		elseif ($areaB > $areaA && $areaB > $areaC) return "Треугольник B, S = {$areaB}";
 		elseif ($areaC > $areaA && $areaC > $areaB) return "Треугольник C, S = {$areaC}";
 		else return '';
 	};
 	
-	$treygolnikA = array();
-	$treygolnikB = array();
-	$treygolnikC = array();
-
 
 	/** Треугольник 1 */
-	array_push($treygolnikA,(int)htmlspecialchars($_POST['gip_1'])); /** Гипотинуза */
-	array_push($treygolnikA,(int)htmlspecialchars($_POST['ygolA_1'])); /** Катет А */
+	$gip = (int)htmlspecialchars($_POST['gip_1']); /** Гипотинуза */
+	$ygol = (int)htmlspecialchars($_POST['ygolA_1']); /** Угол А */
 
 	/** Треугольник 2 */
-	array_push($treygolnikB,(int)htmlspecialchars($_POST['katetA_2'])); /** Угол B */
-	array_push($treygolnikB,(int)htmlspecialchars($_POST['ygolB_2'])); /** Высота H */
+	$katetA = (int)htmlspecialchars($_POST['katetA_2']); /** Катет А*/
+	$ygolB_2 = (int)htmlspecialchars($_POST['ygolB_2']); /** Угол B */
 
 	/** Треугольник 3 */
-	array_push($treygolnikC,(int)htmlspecialchars($_POST['visotaH_3'])); /** Высота H */
-	array_push($treygolnikC,(int)htmlspecialchars($_POST['ygolY_3'])); /** Угол Y */
+	$visotaH_3 = (int)htmlspecialchars($_POST['visotaH_3']); /** Высота H */
+	$ygolY = (int)htmlspecialchars($_POST['ygolY_3']); /** Угол Y */
 
-	$isCorrect = sizeof($treygolnikA) === 2 && sizeof($treygolnikB) === 2
-				&& sizeof($treygolnikC) === 2;
+	$isCorrect = gettype($gip) === "integer" && gettype($ygol) === "integer" && 
+				gettype($katetA) === "integer" && gettype($ygolB_2) === "integer" && 
+				gettype($visotaH_3) === "integer" && gettype($ygolY) === "integer";
 
 	if ($isCorrect){
 
 		$ygolB_1 = 90 - $ygolA_1;
-		$kaktetA_1 = $treygolnikA[0] * sin($treygolnikA[1]);
-		$kaktetB_1 = $treygolnikA[0] * sin($kaktetB_1);
-		$areaA = getArea($kaktetA_1, $kaktetB_1);
+		$kaktetA_1 = $gip * sin($ygol);
 
-		$ygolA_2 = 180 - (90 + $treygolnikB[1]);
-		$gipotenyza_2 = $treygolnikB[0] / sin($ygolA_2);
-		$katetB_2 = $gipotenyza_2 * sin($treygolnikB[1]);
-		$areaB = getArea($treygolnikB[0], $katetB_2);
+		$kaktetB_1 = $gip * sin($kaktetA_1);
+		$areaA =  0.5 * ($kaktetA_1 + $kaktetB_1) * sqrt($kaktetA_1 * $kaktetB_1);
 
-		$gipotenyza_3 = $treygolnikC[0] * sin($treygolnikC[1]);
+		$ygolA_2 = 180 - (90 + $ygolB_2);
+		$gipotenyza_2 = $katetA / sin($ygolA_2);
+		$katetB_2 = $gipotenyza_2 * sin($ygolB_2);
+		$areaB = 0.5 * ($katetA * $katetB_2);
+
+		$gipotenyza_3 = $visotaH_3 * sin($ygolY);
 		$areaC = 0;
 
-
+		echo "<p>A: {$areaA};</p>";
+		echo "<p>B: {$areaB};</p>";
+		echo "<p>C: {$areaC}.</p>";
 	};
 ?>
 
@@ -63,11 +59,11 @@
 		<input placeholder="Гипотенуза" required="" type="number" name="gip_1" /> <br/>
 		<input placeholder="Угол А" required="" type = "number" name = "ygolA_1" /> <br/>
 		<p>Треугольник 2:</p>
-		<input placeholder="Угол А" required="" type = "number" name = "katetA_2" /> <br/>
-		<input placeholder="Угол В" required="" type = "number" name = "ygolB_2" /> <br/>
+		<input placeholder="Катет А"  required="" type = "number" name = "katetA_2" /> <br/>
+		<input placeholder="Угол В"  required="" type = "number" name = "ygolB_2" /> <br/>
 		<p>Треугольник 3_1:</p>
 		<input placeholder_1="Высота Н" required="" type = "number" name = "visotaH_3" /> <br/>
-		<input placeholder="Угол Y" required type = "number" value = "90" name = "ygolY_3" disabled /> <br/>
+		<input placeholder="Угол Y" required type = "number" value = "90" name = "ygolY_3" /> <br/>
 		<br/>
 		<input type="submit" value = 'Выяснить'>
 		<p><?php if ($isCorrect) echo getResult($areaA, $areaB, $areaC); ?></p>

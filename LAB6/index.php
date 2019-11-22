fceb93c42d67f080ee6949e73af4c0c32499761e
themafia98 <gaara33377@gmail.com>
2019-11-22 17:33:15 +0300

    some search

--- LAB6/index.php
+++ LAB6/index.php
@@ -1,8 +1,18 @@
 <?php
 
 $arrayLinear = [5,7,8,"car", 43, 99, 'hi', '543', 221];
-$searchItemLinear = htmlspecialchars($_POST['searchItemLinear']);
-$searchItemBinary = htmlspecialchars($_POST['searchItemBinary']);
+$searchItemLinear = isset($_POST['searchItemLinear']) ? 
+    htmlspecialchars($_POST['searchItemLinear']) : null;
+$searchItemBinary = isset($_POST['searchItemLinear']) ? 
+    htmlspecialchars($_POST['searchItemLinear']) : null;
+$directSearchString = isset($_POST['directSitrng']) ? 
+    htmlspecialchars($_POST['directSitrng']) : "Привет мир, как дела?";
+$searchArticle = isset($_POST['searchDirectArticle']) ? 
+    htmlspecialchars($_POST['searchDirectArticle']) : "мир";
+$searchStringKMP = isset($_POST['searchStringKMP']) ?
+    htmlspecialchars($_POST['searchStringKMP']) : "Привет мир, как дела!!!";
+$searchArticleKMP = isset($_POST['searchArticleKMP']) ?
+    htmlspecialchars($_POST['searchArticleKMP']) : "дела";
 
 
 function linearSearch($searchItemLinear, $arrayLinear){
@@ -39,17 +49,46 @@ function binarySearch($value, $arrayBinary){
 }
 
 function directSearch($string, $searchString){
-    $result = null;
+    $isFind = "false";
+    $searchStringCounter = 0;
 
-    $list = preg_split("//u",$string, -1);
-    $counter = count($list);
+    $stringList = preg_split("//u",$string, -1);
+    $searchList = preg_split("//u", $searchString, -1);
 
-    for ($i = 0; $i < count($list); $i++){
+    $counterStringList = count($searchList);
 
+    for ($i = 0; $i < count($stringList); $i++){
+        if ($searchStringCounter === $counterStringList - 1){
+            $isFind = "true";
+            break;
+        }
+        if ($stringList[$i] === $searchList[$searchStringCounter]){
+            $searchStringCounter += 1;
+        }
     }
+    return $isFind;
 }
 
-directSearch("Привет мир!", "ивет");
+function searchKMP($searchString, $string){
+    $isFind = "false";
+
+    $stringList = preg_split("//u",$string, -1);
+    $searchList = preg_split("//u", $searchString, -1);
+    
+    for ($i = 0; $i < count($stringList); $i++){
+        for ($j = 0; $j < count($searchList); $j++){
+            if (!$searchList[$j]) {
+                return $i;
+            }
+            if ($stringList[$i + $j]  !== $stringList[$j]){
+                $isFind = "true";
+                break;
+            }
+        }
+        
+    }
+    return $isFind;
+}
 
 
 ?>
@@ -87,12 +126,54 @@ directSearch("Привет мир!", "ивет");
         <span>
             <p>Search item index: 
                 <?php 
-                    if ($searchItemBinary) echo binarySearch($searchItemBinary, [1,2,3,4,5,6,7,8,9]);
+                    echo binarySearch($searchItemBinary, [1,2,3,4,5,6,7,8,9]);
                 ?>
             </p>
             <span>
             Search item:
                 <input name = "searchItemBinary" value = "0" type = "text" />
+            </span>
+        </span>
+        <input type = 'submit' value = 'search' />
+    </form>
+    <p>3. Direct search:</p>
+    <form method = "POST" action = 'index.php'>
+       <span> Search string: <?php print_r($directSearchString); ?> </span><br/>
+       <span>String for search: <?php print_r($searchArticle) ?></span>
+        <span>
+            <p>Search result: 
+                <?php 
+                   echo directSearch($directSearchString, $searchArticle);
+                ?>
+            </p>
+            <span>
+            String for search:  
+                <input name = 'directSitrng' 
+                    placeholder="Привет мир, как дела?" 
+                    style = "margin-bottom: 4px;" 
+                    type = 'text' 
+                />
+            </span> <br/>
+            <span>
+            Search item:
+                <input name = "searchDirectArticle"  type = "text" />
+            </span>
+        </span>
+        <input type = 'submit' value = 'search' />
+    </form>
+    <p>4. Search KMP:</p>
+    <form method = "POST" action = 'index.php'>
+       <span> Search string: <?php print_r($searchStringKMP); ?> </span><br/>
+       <span>String for search: <?php print_r($searchArticleKMP) ?></span>
+        <span>
+            <p>Search result: 
+                <?php 
+                   echo searchKMP($searchStringKMP, $searchArticleKMP);
+                ?>
+            </p>
+            <span>
+            Search item:
+                <input name = "searchArticleKMP"  type = "text" />
             </span>
         </span>
         <input type = 'submit' value = 'search' />

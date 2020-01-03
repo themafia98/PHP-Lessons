dbaf9e88b4b585cdad6d19b683856459fc56386f
themafia98 <gaara33377@gmail.com>
2020-01-03 23:57:45 +0300

    some search fix

--- LAB6/search/index.php
+++ LAB6/search/index.php
@@ -13,6 +13,10 @@ $searchStringKMP = isset($_POST['searchStringKMP']) ?
     htmlspecialchars($_POST['searchStringKMP']) : "Привет мир, как дела!!!";
 $searchArticleKMP = isset($_POST['searchArticleKMP']) ?
     htmlspecialchars($_POST['searchArticleKMP']) : "дела";
+$searchTemplateBM = isset($_POST["searchTemplateBM"]) ?
+    htmlspecialchars($_POST["searchTemplateBM"]) : "как";
+$searchSourceBM = isset($_POST["searchSourceBM"]) ?
+    $_POST["searchSourceBM"] : "Привет мир, как дела!!!";
 
 
 function linearSearch($searchItemLinear, $arrayLinear){
@@ -69,42 +73,89 @@ function directSearch($string, $searchString){
     return $isFind;
 }
 
-function searchKMP($string, $searchString){
+function searchKMP(string $source, string $searchString){
 
-    $prefix[0] = 0;
-    $counterString = mb_strlen($string);
-    $counterSearch = mb_strlen($searchString);
+    $matches = [];
+    $l_text = strlen($source);
+    $l_str = strlen($searchString);
+    $i = $j = 0;
 
-    for ($i = 1, $j = 0; $i < $counterSearch; $i++){
-        while($j > 0 && $searchString[$j] != $searchString[$i]){
-            $j = $prefix[$j - 1];
+    function getPrefix(string $data):array {
+        $dataLength = strlen($data);
+
+        $prefixLength = 0;
+        $index = 1;
+        $prefixArray = [];
+        array_push($prefixArray,0); // init
+
+            while ($index < $dataLength) {
+                if ($data[$index] === $data[$prefixLength]) {
+                    $dataLength++;
+                    $prefixArray[$index] = $dataLength;
+                    $index++;
+                } else {
+                    if ($prefixLength !== 0) {
+                        $prefixLength = $prefixArray[$prefixLength - 1];
+                    } else {
+                        $prefixArray[$index] = 0;
+                        $index++;
+                    }
+                }
+            }
+            return $prefixArray;
+    }
+
+
+    $prefix = getPrefix($source);
+
+    var_dump($prefix);
+    while ($i < $l_str) {
+        if ($source[$j] === $searchString[$i]) {
+            $j++;
+            $i++;
         }
 
-        $searchString[$j] == $searchString[$i] && $j++;
-        $prefix[$i] = $j;
-
-        if ($i + 1 < $counterSearch){
-            for($i = 0, $j = 0; $i < $counterString; $i++){
-
-                while($j > 0 && $searchString[$j] != $string[$i]){
-                    $j = $prefix[$j - 1];
-                }
-        
-                $searchString[$j] == $string[$i] && $j++;
-                if($j == $counterSearch) return "true";
-            }
+        if ($j == $l_text) {
+            array_push($matches, $i - $j);
+            $j = $prefix[$j - 1];
+        } else if ($i < $l_str && $source[$j] !== $searchString[$i]) {
+            if ($j !== 0)
+                $j = $prefix[$j - 1];
+            else
+                $i = $i + 1;
         }
     }
-    return "false";
+    return $matches;
 }
 
-function searchBM($source, $template){
-    $lengthSource = mb_strlen($source);
-    return $lengthSource;
+function searchBM(string $template, string $source){
+    $lengthSource = strlen($source);
+    $lengthTemplate = strlen($template);
+
+    function getCharTable(string $data){
+        $dataLength = strlen($data);
+        $table = [];
+        for ($i = 0; $i < $dataLength; $i++) {
+            $table[$data[$i]] = $dataLength - $i - 1;
+        }
+        return $table;
+    }
+
+    $table = getCharTable($source);
+
+    for ($i = $lengthSource - 1; $i < $lengthTemplate;) {
+        $t = $i;
+        for ($j = $lengthSource-1; $source[$j] === $template[$i]; $j--, $i--) {
+            if($j === 0) return $i;
+        }
+        $i = $t;
+        if(array_key_exists($template[$i], $table))
+            $i = $i + max($table[$template[$i]], 1);
+        else
+            $i += $lengthSource;
+    }
+    return false;
 }
-
-
-
 ?>
 
 
@@ -117,7 +168,6 @@ function searchBM($source, $template){
     <title>Algorithms php. Lab 6.</title>
 </head>
 <body>
-<?php searchBM("Hellaxc zxz o", null); ?>
     <h3>Lab 6</h3>
     <p>1. Linear search:</p>
     <?php if ($arrayLinear || $arrayLinear === 0) print_r($arrayLinear) ?>
@@ -183,7 +233,8 @@ function searchBM($source, $template){
         <span>
             <p>Search result: 
                 <?php 
-                   echo searchKMP($searchStringKMP,$searchArticleKMP);
+                  // $result = searchKMP($searchStringKMP,$searchArticleKMP);
+                   //var_dump($result);
                 ?>
             </p>
             <span>
@@ -193,6 +244,33 @@ function searchBM($source, $template){
         </span>
         <input type = 'submit' value = 'search' />
     </form>
+<p>5. Search BM:</p>
+<form method = "POST" action = 'index.php'>
+    <span> Search string: <?php print_r($searchTemplateBM); ?> </span><br/>
+    <span>String for search: <?php print_r($searchSourceBM) ?></span>
+    <span>
+            <p>Search result:
+                <?php
+                $result = searchBM($searchSourceBM,$searchTemplateBM);
+                if ($result) echo $result;
+                else echo "not found";
+                ?>
+            </p>
+            <span>
+                String for search:
+                    <input name = 'searchSourceBM'
+                           value="Привет мир, как дела?"
+                           style = "margin-bottom: 4px;"
+                           type = 'text'
+                    />
+            </span> <br/>
+            <span>
+            Search item:
+                <input name = "searchTemplateBM"  type = "text" />
+            </span>
+        </span>
+    <input type = 'submit' value = 'search' />
+</form>
     
 </body>
 </html>
